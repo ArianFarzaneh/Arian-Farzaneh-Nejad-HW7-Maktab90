753be47b48c32b338489e372744b49badc82f66c
Arian Farzanehnejad <afn2681@gmail.com>
2023-01-29 22:41:13 +0330

    add the answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 'use strict'
+'use strict'
+let values = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+let uniqe=(values)=>
+{
+    let set=new Set(values)//tabdil array be set chon set uniqe ast
+    let arr=Array.from(set)//tabdil mojadad set be array
+    console.log(arr);
+}
+uniqe(values);
