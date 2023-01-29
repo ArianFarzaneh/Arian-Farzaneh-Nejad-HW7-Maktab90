46342061c1bdb65c166acff416e7943ed97ff175
Arian Farzanehnejad <afn2681@gmail.com>
2023-01-29 23:32:44 +0330

    add the answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 'use strict'
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+let aclean=(arr)=>
+{
+    let map = new Map()//sakhtan yek map
+    for(let i of arr)
+    {
+        let uniqe=i.toLowerCase().split("").sort().join('')
+        map.set(uniqe,i)
+    }
+    let arr2=Array.from(map.values());
+    console.log(arr2);
+}
+aclean(arr);
