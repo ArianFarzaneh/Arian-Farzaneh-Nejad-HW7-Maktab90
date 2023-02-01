5f9daad5a43d26d0f0a9d76a344612556c4a7854
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-01 12:35:38 +0330

    fix: delete wrong js code

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,2 @@
+//q6
 'use strict'
-/*dalil in etefagh in ast ke
-method map.keys() yek array be ma bar nemigardanad
-pas rahe hal hal kardan moshkel in ast ke 
-map ra az tarigh estefade az Array.form()
-barabar yek array gharar bedahim
-*/
-//shekl dorost:
-let map = new Map();
-map.set("name", "John");
-let keys =new Array()
-keys= Array.from(map.keys());//inja ba estefade az Array.from() map ra be array tabdil kardim
-keys.push("more");
-console.log(keys);
